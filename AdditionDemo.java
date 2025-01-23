f9277a4930cb11087a37622b8637aeaef07b3092
babitakc <babitakc674@gmail.com>
2025-01-23 07:56:11 +0545

    Program to demo

---
+++ AdditionDemo.java
@@ -0,0 +1,19 @@
+/Custom package
+package myPackages;
+// File: Addition.java
+public class Addition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+//Main file which imports custom package
+// File: AdditionDemo.java
+import myPackages.Addition;
+public class AdditionDemo {
+    public static void main(String[] args) {
+        Addition adds = new Addition();
+        int num1 = 10, num2 = 5;
+        System.out.println("Sum: " + adds.add(num1, num2));
+      
+    }
+}
